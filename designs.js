c81b6732e81a2405d15ba566ded5e4b796fec0f8
sod2003 <sod2003@gmail.com>
2022-12-12 23:08:50 -0500

    Finishing project to rubric standard

--- designs.js
+++ designs.js
@@ -1,43 +1,56 @@
-// Select color input
-const color = document.getElementById('colorPicker');
-// Select size input
-const height = document.getElementById('inputHeight');
-const width = document.getElementById('inputWidth');
-// Select Pixel Canvas and Submit Button
-const pixelCanvas = document.getElementById('pixelCanvas');
-const submitButton = document.getElementById('submit');
+document.addEventListener('DOMContentLoaded', function () {
+    // Select color input
+    const color = document.getElementById('colorPicker');
 
-// Function to clear Pixel Canvas
-function removeGrid() {
-    while (pixelCanvas.firstChild) {
-        pixelCanvas.removeChild(pixelCanvas.firstChild);
-    }
-}
+    // Select size input
+    const height = document.getElementById('inputHeight');
+    const width = document.getElementById('inputWidth');
 
-// Set background color of element
-function setBackgroundColor(id) {
-    console.log(id);
-    let cell = document.getElementById(id);
-    cell.style.backgroundColor = color.value;
-}
+    // Select Pixel Canvas and Submit Button
+    const pixelCanvas = document.getElementById('pixelCanvas');
+    const submitButton = document.getElementById('submit');
 
-// When size is submitted by the user, call makeGrid()
-function makeGrid() {
-    removeGrid();
-    for (let i = 1; i <= parseInt(height.value); i++) {
-        const row = document.createElement('tr');
-        pixelCanvas.appendChild(row);
-        for (let j = 1; j <= parseInt(width.value); j++) {
-            const cell = document.createElement('td');
-            cell.id = "r" + i + "c" + j;
-            row.appendChild(cell);
-            cell.addEventListener('click', setBackgroundColor(cell.id));
+    // Function to clear Pixel Canvas
+    function removeGrid() {
+        while (pixelCanvas.firstChild) {
+            pixelCanvas.removeChild(pixelCanvas.firstChild);
         }
     }
-}
 
-// Event Listener for the submit button
-submitButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    makeGrid();
+    // Set background color of element
+    function setBackgroundColor(cell) {
+        cell.style.backgroundColor = color.value;
+    }
+
+    // When size is submitted by the user, call makeGrid()
+    function makeGrid() {
+        removeGrid();
+        for (let i = 1; i <= parseInt(height.value); i++) {
+            const row = document.createElement('tr');
+            pixelCanvas.appendChild(row);
+            for (let j = 1; j <= parseInt(width.value); j++) {
+                const cell = document.createElement('td');
+                row.appendChild(cell);
+            }
+        }
+    }
+
+    // Attach event listener to cells
+    function cellListeners() {
+        const cells = document.getElementsByTagName('td');
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].addEventListener('click', function(event) {
+                if (event.target.nodeName === "TD") {
+                    setBackgroundColor(this);
+                }
+            });
+        }
+    }
+
+    // Event Listener for the submit button
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        makeGrid();
+        cellListeners();
+    });
 });
